ce35a11017aca97f8cf981d6378dacfe6c359e99
Miguel Santos <msantos@lisboacentralhostel.com>
2026-04-27 11:45:56 +0100

    fix: track runtime config

---
+++ config.js
@@ -0,0 +1,4 @@
+window.APP_CONFIG = {
+  SUPABASE_URL: "https://mxrerkflidphfeazdiov.supabase.co",
+  SUPABASE_ANON_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im14cmVya2ZsaWRwaGZlYXpkaW92Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzU4MjYyOTYsImV4cCI6MjA5MTQwMjI5Nn0.M9C8WaisW69LBNwhcR0p71UvYhh0aqMSbNBkkyH1_IE",
+};
